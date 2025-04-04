05b569ae2a6be37620c63a9a2f8818e5e50792c1
desabi <desabi@live.com>
2025-04-04 16:38:35 -0600

    feat: demo-a, base project

---
+++ graphql-demo-a/src/main/java/com/desabisc/guide/api/graphql/GraphqlDemoAApplication.java
@@ -0,0 +1,13 @@
+package com.desabisc.guide.api.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlDemoAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlDemoAApplication.class, args);
+	}
+
+}
---
+++ graphql-demo-a/src/test/java/com/desabisc/guide/api/graphql/GraphqlDemoAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabisc.guide.api.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDemoAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
