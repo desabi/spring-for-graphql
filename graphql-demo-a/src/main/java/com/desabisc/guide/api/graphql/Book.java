2e40c52debe0d9814cfe5df01a77c5e35254e10c
desabi <desabi@live.com>
2025-04-05 14:38:11 -0600

    feat: demo-a, read book by id

--- graphql-demo-a/src/main/java/com/desabisc/guide/api/graphql/Book.java
+++ graphql-demo-a/src/main/java/com/desabisc/guide/api/graphql/Book.java
@@ -1,14 +1,22 @@
 package com.desabisc.guide.api.graphql;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public record Book(Integer id, String name, Integer pageCount) {
   public static List<Book> bookList = List.of(
       new Book(1, "A Tale of Two Cities", 560),
       new Book(2, "The Little Prince", 146),
-      new Book(3, "The Alchemist ", 315),
+      new Book(3, "The Alchemist", 315),
       new Book(4, "Harry Potter and the Philosopher's Stone", 290),
       new Book(5, "And Then There Were None", 472)
   );
+
+  public static Optional<Book> getBookById(Integer id) {
+    return bookList.stream()
+        .filter(book -> book.id.equals(id))
+        .findFirst();
+
+    //return bookList.get(id);
+  }
 }
--- graphql-demo-a/src/main/java/com/desabisc/guide/api/graphql/BookController.java
+++ graphql-demo-a/src/main/java/com/desabisc/guide/api/graphql/BookController.java
@@ -1,6 +1,8 @@
 package com.desabisc.guide.api.graphql;
 
 import java.util.List;
+import java.util.Optional;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -10,5 +12,10 @@ public class BookController {
   @QueryMapping
   public List<Book> books() {
     return Book.bookList;
+  }
+
+  @QueryMapping
+  public Optional<Book> bookById(@Argument Integer id) {
+    return Book.getBookById(id);
   }
 }
